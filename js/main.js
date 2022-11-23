c4088eb1e91a24cc93ee19aa26558047e545c34c
NekrOnFire <nekrasovonfire@gmail.com>
2022-11-23 22:36:25 +0400

    удаляет ненужные импорты

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 import { renderPictures } from './pictures.js';
 import { showAlert } from './util.js';
-import './form.js';
-import './scale.js';
-import './effects.js';
 import { getData } from './api.js';
 import { setUserFormSubmit } from './form.js';
 
